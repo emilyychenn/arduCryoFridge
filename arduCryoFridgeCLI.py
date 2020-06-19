ce3d130fd31a7db7669aaea62d053cfd67e58c9e
Stefan A Reinsberg <stefan@phas.ubc.ca>
2020-06-18 19:12:10 -0700

    First step to complete issue #3

---
+++ arduCryoFridgeCLI.py
@@ -0,0 +1,14 @@
+"""
+Usage:
+   arduCryoFridgeCLI.py configure [--on=<ontime>] [--off=<offtime>] [--on=<ontime>]
+   arduCryoFridgeCLI.py switch [--on | --off] [--now | --delay=<delay>]
+   arduCryoFridgeCLI.py -s | --status
+   arduCryoFridgeCLI.py -h | --help
+   arduCryoFridgeCLI.py -q
+
+Options:
+  --delay=<delay>  start on/off cycle in delay [default=0] minutes.
+  -s --status      Read out and report PT410 status
+  -h --help        Show this screen.
+  -q               Query program version + version run on the arduino
+"""
