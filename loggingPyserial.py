b4cebae59ed94ffda166c890046af47d421083f6
Emily Chen <emilylynnchen@gmail.com>
2020-07-01 15:17:35 -0400

    functional logging system

--- loggingPyserial.py
+++ loggingPyserial.py
@@ -5,9 +5,11 @@ from datetime import date
 import time
 import os
 
-usbPort = '/dev/cu.usbmodem14201'
+usbPort = '/dev/cu.usbmodem14101'
 baud = 9600
 ser = serial.Serial(usbPort, baud)
+
+print(serial.__version__)
 
 # log all button interrupts as warnings
 logger = logging.getLogger(__name__)
@@ -20,10 +22,19 @@ today = date.today()
 file = today.strftime('%m-%d-%Y')
 logging.basicConfig(filename=(file + '.log'), level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+
+# TODO: add condition to remove time.sleep?? needs to check if there has been a change, and then
+# only display when there has been a change OR every hour
 try:
     while True:
-        time.sleep(0.05)
-        logging.warning(ser.readline(ser.inWaiting()))
-        print("logged status changed")
+        if (ser.in_waiting != 0):
+            time.sleep(0.05)
+            ser.flush() # wait until all data is written
+            warn = ser.readline(ser.in_waiting).strip(('\r\n').encode()) #removes '\r\n' at the end of each printed line
+            # warn = ser.readline(ser.inWaiting()).strip(('\r\n').encode()) #removes '\r\n' at the end of each printed line
+            logging.warning(warn)
+            print("logged status changed")
+        else:
+            continue
 except KeyboardInterrupt:
-    print("Logging stopped")
+    print("\nLogging stopped")
