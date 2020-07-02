1d54cfd3289434c361dfed4d25b503cecbde4be5
Emily Chen <emilylynnchen@gmail.com>
2020-07-02 16:04:26 -0400

    functional python service listening to arduino serial reports; resolves issue #5

--- loggingPyserial.py
+++ loggingPyserial.py
@@ -1,15 +1,13 @@
 import serial
 import serial.tools.list_ports
 import logging
-from datetime import date
+from datetime import date, time, timedelta, datetime
 import time
 import os
 
-usbPort = '/dev/cu.usbmodem14101'
+usbPort = '/dev/cu.usbmodem14201'
 baud = 9600
 ser = serial.Serial(usbPort, baud)
-
-print(serial.__version__)
 
 # log all button interrupts as warnings
 logger = logging.getLogger(__name__)
@@ -23,18 +21,32 @@ file = today.strftime('%m-%d-%Y')
 logging.basicConfig(filename=(file + '.log'), level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
-# TODO: add condition to remove time.sleep?? needs to check if there has been a change, and then
-# only display when there has been a change OR every hour
+# display status of buttons every hour on the hour
+def giveStatusUpdate():
+    x = datetime.today()
+    y = x.replace(day=x.day, hour=x.hour, minute=58, second=0, microsecond=0)
+    delta_t = y.minute-x.minute
+    delta_str = str(delta_t)
+        
+    if(float(delta_str) <= 1.0):
+        t = time.localtime()
+        currentTime = time.strftime("%H:%M:%S", t)
+        statusUpdate = ("Status Update: " + currentTime)
+        logging.info(statusUpdate)
+        print(statusUpdate)
+
+
 try:
     while True:
         if (ser.in_waiting != 0):
             time.sleep(0.05)
             ser.flush() # wait until all data is written
-            warn = ser.readline(ser.in_waiting).strip(('\r\n').encode()) #removes '\r\n' at the end of each printed line
-            # warn = ser.readline(ser.inWaiting()).strip(('\r\n').encode()) #removes '\r\n' at the end of each printed line
+            warn = ser.readline(ser.in_waiting).strip(('\r\n').encode()) #removes '\r\n' at the end of each line
             logging.warning(warn)
             print("logged status changed")
+            giveStatusUpdate()
         else:
             continue
 except KeyboardInterrupt:
     print("\nLogging stopped")
+
